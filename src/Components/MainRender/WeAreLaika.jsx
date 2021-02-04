41b25d3f61dcd2b2aaf3d6b31aa09b173f236695
Danail Ivanovski <danail1992@gmail.com>
2021-02-04 20:06:41 +0100

    added .eslintcache in gitignore

--- src/Components/MainRender/WeAreLaika.jsx
+++ src/Components/MainRender/WeAreLaika.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import laika1 from "./img/Laika screenshots/Laika1.png";
+// import laika1 from "./img/Laika screenshots/Laika1.png";
 
 class WeAreLaika extends Component {
   constructor() {
@@ -10,7 +10,7 @@ class WeAreLaika extends Component {
   render() {
     return (
       <div className="WeAreLaika">
-        <img src={laika1} alt="WeAreLaika" className="w-100 d-block" />
+        {/* <img src={laika1} alt="WeAreLaika" className="w-100 d-block" /> */}
       </div>
     );
   }
