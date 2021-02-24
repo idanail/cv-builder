64b57a1b0a7254def6ac9f4fb435b51ab258bfff
Danail Ivanovski <danail1992@gmail.com>
2021-02-25 00:33:49 +0100

    fixed several inaccurately resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Categories from "./Components/Categories/Categories";
 import LandingPage from "./Components/Landing page/LandingPage";
 import MainRender from "./Components/MainRender/MainRender";
+import Popup from "./Components/MainRender/Popup";
+import NotFound from "./Components/NotFound/NotFound";
+import { DataContextProvider } from "./context/CvContext";
 
 function App() {
   return (
-    <div className="App container-fluid">
-      <Router>
-        <Switch>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/categories" exact component={Categories} />
-          <Route path="/categories/:url" component={MainRender} />
-        </Switch>
-      </Router>
-    </div>
+    <Container fluid className="App">
+      <DataContextProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/popup" component={Popup} />
+            <Route path="/categories" exact component={Categories} />
+            <Route path="/categories/:url" component={MainRender} />
+            <Route path="/notfound" component={NotFound} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </DataContextProvider>
+    </Container>
   );
 }
 
